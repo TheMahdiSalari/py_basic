e98c8bd0cb1db09c87c37d8d0209de954f2ff555
Mahdi Salari <8002946-Redixgp@users.noreply.gitlab.com>
2023-12-18 11:51:16 +0330

    Converting students' grades from quantitative to qualitative

---
+++ prac2_2.py
@@ -0,0 +1,13 @@
+
+######## 2
+score = float(input("Please enter student's score: "))
+if score <= 9.9 :
+    print("FAIL")
+elif score <= 15 :
+    print("GOOD")
+elif score <= 18 :
+    print("VERY GOOD")
+elif score <= 20 :
+    print("EXCELLENT")
+else :
+    print("INVALID GRADE")
