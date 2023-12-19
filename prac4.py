e97908f395c1879c20a85b1ec516b2a156b42ee4
Mahdi Salari <8002946-Redixgp@users.noreply.gitlab.com>
2023-12-19 18:41:35 +0330

    The sum of squares of numbers from 1 to n and the square root of the sum of squares

--- prac4.py
+++ prac4.py
@@ -1,12 +1,12 @@
 from math import sqrt
-
-n = int(input("Enter the number of range = "))
+n = int(input("Enter the number of range : "))
 s = 0
 ss = 0
-for i in range(n+1):
+for i in range(n+1) :
     s = s + (i**2)
     ss = sqrt(s)
-print("(Sum of squares of numbers)--->")
-print("S = ",s)
-print("(The sqrt of the Sum of squares of numbers)---->")
-print("SS = ",ss)
+print("(Sum of squares of numbers)----->")
+print("S = ", s)
+print("(The sqrt of the sum of squares of numbers)----->")
+print("SS = ", ss)
+
