bb0fe0348347785adfd516678fdfabf4185dcdfa
Mahdi Salari <8002946-Redixgp@users.noreply.gitlab.com>
2023-12-19 18:40:51 +0330

    Coordinate distance between two points

--- prac3_3.py
+++ prac3_3.py
@@ -1,12 +1,12 @@
 from math import *
 
-
 def distance(x1,x2,y1,y2):
-    d = sqrt((x2-x1)**2 + (y2-y1)**2)
+    d = sqrt((x1-x2)**2 + (y1-y2)**2)
     print("distance = ", d)
 
-x1 =float(input("enter x1 point : "))
-y1 =float(input("enter y1 point : "))
-x2 =float(input("enter x2 point : "))
-y2 =float(input("enter y2 point : "))
-distance(x1, y1, x2, y2)
+x1 = float(input("enter x1 point : "))
+y1 = float(input("enter y1 point : "))
+x2 = float(input("enter x2 point : "))
+y2 = float(input("enter y2 point : "))
+distance(x1 , y1 , x2 , y2)
+
