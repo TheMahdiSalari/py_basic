b78b83d2c39b88d4f492db6dae28b97bc8344bc5
Mahdi Salari <8002946-Redixgp@users.noreply.gitlab.com>
2023-12-19 13:39:50 +0330

    Obtaining the roots of the equation

--- prac2_1.py
+++ prac2_1.py
@@ -1,22 +1,18 @@
 from math import *
 
-####### 1
-print("(ax**2) + (bx) + c =0")
-a = float(input("please enter number for a : "))
-b = float(input("please enter number for b : "))
-c = float(input("please enter number for c : "))
-delta = pow(b , 2) - (4*a*c)
-if delta > 0:
+print("(a**2) + (b*x) + c = 0")
+a = float(input("please enter number for a :"))
+b = float(input("please enter number for b :"))
+c = float(input("please enter number for c :"))
+delta = pow(b,2) - (4*a*c)
+if delta > 0 :
     print("The equation has two roots")
-    x1 = ((-b) + sqrt(delta)) / (2*a)
-    x2 = ((-b) - sqrt(delta)) / (2*a)
-    print("The first root has : ",x1,"\n","The second root has : ",x2)
+    x1 = ((-b) + sqrt(delta)/(2*a))
+    x2 = ((-b) - sqrt(delta)/(2*a))
+    print("the first root is : ",x1,"\n","The second root is : ",x2)
 elif delta == 0 :
     print("The equation has one root")
     x = (-b) / (2*a)
-    print("The root has : ",x)
-elif delta < 0:
+    print("The root is : ",x)
+elif delta < 0 :
     print("The equation has no real roots")
-
-
-
