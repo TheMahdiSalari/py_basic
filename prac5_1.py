b605a3df779ee136286ed8ca6bd693a957928f30
Mahdi Salari <8002946-Redixgp@users.noreply.gitlab.com>
2023-12-22 16:02:09 +0330

    Working with lists

--- prac5_1.py
+++ prac5_1.py
@@ -1,6 +1,6 @@
 x = [1 , -5 , 9 , 23 , 12 , 1]
-x[4:4] = ["reza"]   #add between 23 and 12
-x[2:4] = []         #delete member of list
-x[0] = "ali"
-x[4] = "ali"
-print(x)
+x[4:4] = ["reza"]       #add between 23 and 12
+x[2:4] = []             #delete member of list
+x[0] = "ali"            #replace
+x[4] = "ali"            #replace
+print("The new list = " , x)
